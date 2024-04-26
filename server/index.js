b808d15c3f72b1cbf6b7883e02aba3e40b35b92e
Debananda <kumardebananda111@gmail.com>
2024-04-26 13:59:32 +0530

    created user schema and model

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then ( () => {
+        console.log("mongodb is connected");
+    }) 
+ .catch((err) => {
+    console.log("err");
+ });
 
 const app = express();
-
+// mdjohnson@123
 app.listen(3000, () => {
     console.log("server is running 3000");
 });
+// mongodb+srv://kumardebananda111:<password>@cluster0.cvn1vov.mongodb.net/
+// mongodb+srv://kumardebananda111:mdjohnson@789@cluster0.cvn1vov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+
+// mongodb+srv://kumardebananda111:KdslvLFUbsw0b0Wo@cluster0.mzle3jy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
